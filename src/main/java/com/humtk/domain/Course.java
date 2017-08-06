67e2c345a40a5446780f4e83eb8c9c2d440c62dc
Kursat Aktas <kursat.ce@gmail.com>
2017-08-06 21:31:33 +0600

    - Domain Eklemeler

---
+++ src/main/java/com/humtk/domain/Course.java
@@ -0,0 +1,35 @@
+package com.humtk.domain;
+
+/**
+ * Created by kursat on 6.8.2017.
+ */
+public class Course {
+
+    private long id;
+    private String name;
+    private String course_code;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse_code() {
+        return course_code;
+    }
+
+    public void setCourse_code(String course_code) {
+        this.course_code = course_code;
+    }
+}
