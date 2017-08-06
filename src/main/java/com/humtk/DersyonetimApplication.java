f8c4ab1f8b4e6b3e68f3d80edc97ae2a3a2ba8f2
Kursat Aktas <kursat.ce@gmail.com>
2017-08-06 18:53:11 +0600

    Initial commit

---
+++ src/main/java/com/humtk/DersyonetimApplication.java
@@ -0,0 +1,12 @@
+package com.humtk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DersyonetimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DersyonetimApplication.class, args);
+	}
+}
