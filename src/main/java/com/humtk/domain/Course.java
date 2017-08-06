6351d06584d93e086d040213970b5b648c8a6dd5
Burcu Iskender <burcuiskender5@gmail.com>
2017-08-06 19:17:06 +0300

    domainler eklendi

--- src/main/java/com/humtk/domain/Course.java
+++ src/main/java/com/humtk/domain/Course.java
@@ -1,11 +1,21 @@
 package com.humtk.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by kursat on 6.8.2017.
  */
+
+@Entity
 public class Course {
 
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private long id;
+
     private String name;
     private String course_code;
 
