cfe3ab95fd8bc89009d6bbd1d8c1319532f18b1b
ArafatYilmaz01 <yilmaz.arafat@gmail.com>
2017-08-14 22:16:07 +0200

    adminpanel-addstudent

--- src/adminpanel/app/dashboard/dashboard.routes.ts
+++ src/adminpanel/app/dashboard/dashboard.routes.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { CourseComponent } from './courses/course.component';
+import { StudentComponent } from './student/student.component';
 import { IconsComponent } from './icons/icons.component';
 import { TableComponent } from './table/table.component';
 import { NotificationsComponent } from './notifications/notifications.component';
@@ -12,6 +13,7 @@ import { UpgradeComponent } from './upgrade/upgrade.component';
 export const MODULE_ROUTES: Route[] =[
     { path: 'home', component: HomeComponent },
     { path: 'courses', component: CourseComponent },
+    { path: 'student', component: StudentComponent },
     { path: 'table', component: TableComponent },
     { path: 'icons', component: IconsComponent },
     { path: 'notifications', component: NotificationsComponent },
@@ -24,6 +26,7 @@ export const MODULE_ROUTES: Route[] =[
 export const MODULE_COMPONENTS = [
     HomeComponent,
     CourseComponent,
+    StudentComponent,
     TableComponent,
     IconsComponent,
     NotificationsComponent,
---
+++ src/adminpanel/app/dashboard/student/student.ts
@@ -0,0 +1,7 @@
+export class Student {
+    id : number;
+    firstName : string;
+    lastName : string;
+    mail : string;
+    studentNo : number;
+}
