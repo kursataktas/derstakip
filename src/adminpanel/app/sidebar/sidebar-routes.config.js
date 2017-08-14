490ee27df07ad73cd495722be64d080b611625a1
İrem <iremozeen@gmail.com>
2017-08-14 17:49:32 +0300

    adminPanel

--- src/adminpanel/app/sidebar/sidebar-routes.config.js
+++ src/adminpanel/app/sidebar/sidebar-routes.config.js
@@ -2,8 +2,7 @@
 exports.ROUTES = [
     { path: 'home', title: 'Anasayfa', icon: 'pe-7s-way', class: '' },
     { path: 'courses', title: 'Ders Ekle', icon: 'pe-7s-note', class: '' },
+    { path: 'student', title: 'Öğrenci Ekle', icon: 'pe-7s-note', class: '' },
     { path: 'table', title: 'Table List', icon: 'pe-7s-note2', class: '' },
-    // { path: 'typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
-    { path: 'icons', title: 'Icons', icon: 'pe-7s-science', class: '' },
 ];
 //# sourceMappingURL=sidebar-routes.config.js.map
