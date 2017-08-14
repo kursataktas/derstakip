89e959800ddc09db23359489d6a10a41f3d509f2
İrem <iremozeen@gmail.com>
2017-08-14 23:19:10 +0300

    adminPanelAddStudent

--- src/adminpanel/app/dashboard/dashboard.routes.js
+++ src/adminpanel/app/dashboard/dashboard.routes.js
@@ -1,6 +1,7 @@
 "use strict";
 var home_component_1 = require('./home/home.component');
 var course_component_1 = require('./courses/course.component');
+var student_component_1 = require('./student/student.component');
 var icons_component_1 = require('./icons/icons.component');
 var table_component_1 = require('./table/table.component');
 var notifications_component_1 = require('./notifications/notifications.component');
@@ -10,6 +11,7 @@ var upgrade_component_1 = require('./upgrade/upgrade.component');
 exports.MODULE_ROUTES = [
     { path: 'home', component: home_component_1.HomeComponent },
     { path: 'courses', component: course_component_1.CourseComponent },
+    { path: 'student', component: student_component_1.StudentComponent },
     { path: 'table', component: table_component_1.TableComponent },
     { path: 'icons', component: icons_component_1.IconsComponent },
     { path: 'notifications', component: notifications_component_1.NotificationsComponent },
@@ -21,6 +23,7 @@ exports.MODULE_ROUTES = [
 exports.MODULE_COMPONENTS = [
     home_component_1.HomeComponent,
     course_component_1.CourseComponent,
+    student_component_1.StudentComponent,
     table_component_1.TableComponent,
     icons_component_1.IconsComponent,
     notifications_component_1.NotificationsComponent,
