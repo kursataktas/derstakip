7cfd17b70b07a5c71db9a8a54dd14f1f260df090
Burcu Iskender <burcuiskender5@gmail.com>
2017-08-19 20:39:07 +0300

    "gereksiz dosyaların bir kısmı temizlendi"

---
+++ src/inspanel/app/classes/instructor.ts
@@ -0,0 +1,4 @@
+export class Instructor {
+    id : number;
+    mail : string;
+}
--- src/inspanel/app/dashboard/dashboard.routes.ts
+++ src/inspanel/app/dashboard/dashboard.routes.ts
@@ -4,30 +4,25 @@ import { HomeComponent } from './home/home.component';
 import { CourseComponent } from './courses/course.component';
 import { IconsComponent } from './icons/icons.component';
 import { TableComponent } from './table/table.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { TypographyComponent } from './typography/typography.component';
 import { MapsComponent } from './maps/maps.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+
 
 export const MODULE_ROUTES: Route[] =[
     { path: 'home', component: HomeComponent },
     { path: 'courses', component: CourseComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'table', component: TableComponent },
     { path: 'icons', component: IconsComponent },
-    { path: 'notifications', component: NotificationsComponent },
-    { path: 'typography', component: TypographyComponent },
     { path: 'maps', component: MapsComponent },
-    { path: 'upgrade', component: UpgradeComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+
 ]
 
 export const MODULE_COMPONENTS = [
     HomeComponent,
     CourseComponent,
+
     TableComponent,
     IconsComponent,
-    NotificationsComponent,
-    TypographyComponent,
-    MapsComponent,
-    UpgradeComponent
+    MapsComponent
+
 ]
